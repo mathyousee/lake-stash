a29ed1bb1c3c156353a438d92ea49c1d588b8d47
Matthew C. Anderson <matthewcanderson@gmail.com>
2025-07-11 03:40:49 +0000

    feat: implement Azure Functions API with message endpoint and update configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/react.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
+  const [data, setData] = useState('');
+
+    useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`../api/mymessage`)).json();
+      setData(text);
+    })();
+  });
 
   return (
     <>
@@ -25,6 +34,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <div>my data is "{data}"</div>;
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
